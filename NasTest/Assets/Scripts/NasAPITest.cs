673667d1a6f97de0db54753f58a61a9be587531f
ch.choi <ccg.1120@daum.net>
2019-07-17 19:23:39 +0900

    1.api 테스트중 2. 로그 기록 패키지 추가

--- NasTest/Assets/Scripts/NasAPITest.cs
+++ NasTest/Assets/Scripts/NasAPITest.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class NasAPITest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void ButtonTest()
     {
-        
+        Debug.Log("Test !!");
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        //StartCoroutine(GetText("http://192.168.50.9:32964/webapi/query.cgi?api=SYNO.API.Info&version=1&method=query&query=all"));
+
+        //StartCoroutine(GetText("http://192.168.50.9:32964/webapi/entry.cgi?api=SYNO.FileStation.Info&version=2&method=get"));
+
+        StartCoroutine(GetText("http://192.168.50.9:32964/webapi/FilStation/info.cgi?api=SYNO.FileStation.CreateFolder&method=create&version=1&folder_path=%2Ftest&name=%3A"));
     }
+
+    IEnumerator GetText(string query)
+    {
+        //UnityWebRequest www = UnityWebRequest.Get("http://myds.com:port/webapi/query.cgi?api=SYNO.API.Info&version=1&method=query&quer y=all");
+        //UnityWebRequest www = UnityWebRequest.Get("http://192.168.50.9:32964/webapi/query.cgi?api=SYNO.API.Info&version=1&method=query&query=all");
+        UnityWebRequest www = UnityWebRequest.Get(query);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+            
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+
+
 }
